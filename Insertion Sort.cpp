8ae0f245caa4bd2fb4197c6c0c1feff6908fa7d5
Bendadiotulada-323 <bendadiotulada@gmail.com>
2026-03-13 14:33:33 +0700

    input jumlah data array if n <=20

--- Insertion Sort.cpp
+++ Insertion Sort.cpp
@@ -1,2 +1,16 @@
 #Include <iostream>
 using namespace std;
+
+int main[20];
+int n;
+
+void input(){
+    while (true){
+        cout << "masukan jumlah data pada array : ";
+        cin >> n;
+
+        if (n <=20){
+            break;
+        }
+    }
+}
